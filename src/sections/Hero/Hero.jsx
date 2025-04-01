9d962324a86cdea33a60a69b49e73fbb75838ba9
Gisela Araújo <94924822+giselaAS@users.noreply.github.com>
2025-04-01 01:41:10 -0300

    add icons and button

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import styles from './HeroStyles.module.css';
 import heroImg from './../../assets/hero-img.png'
 import themeIcon from './../../assets/sun.svg'
+import threadsIcon from './../../assets/twitter-light.svg'
+import githubIcon from './../../assets/github-light.svg'
+import linkedinIcon from './../../assets/linkedin-light.svg'
+import curriculo from './../../assets/Currículo - Gisela.pdf'
 
 function Hero() {
   return (
@@ -15,8 +19,20 @@ function Hero() {
             </h1>
             <h2>Frontend Developer</h2>
             <span>
-                <a href='' target='_blank'></a>
+                <a href='https://www.threads.net/@g154gisa' target='_blank'>
+                    <img src={threadsIcon} alt="Threads icon" />
+                </a>
+                <a href='https://github.com/giselaAS' target='_blank'>
+                    <img src={githubIcon} alt="GitHub icon" />
+                </a>
+                <a href='https://www.linkedin.com/in/gisela-ara%C3%BAjo-066456290/' target='_blank'>
+                    <img src={linkedinIcon} alt="Linkedin icon" />
+                </a>
             </span>
+            <p>With a passing for developing modern React web apps.</p>
+            <a href={curriculo} download>
+                <button className='hover'>Resume</button>
+            </a>
         </div>
     </section>
   )
