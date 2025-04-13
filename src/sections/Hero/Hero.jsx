5ef41dfa7cb655d158a66379135050f52e60bd3e
Gisela Araújo <94924822+giselaAS@users.noreply.github.com>
2025-04-13 16:07:11 -0300

    chore: flexbox

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,21 +1,18 @@
 import styles from './Hero.module.css';
-import heroImg from './../../assets/hero-img.png'
+import heroImg from './../../assets/chatgpt_image_3.png'
 import sun from './../../assets/sun.svg'
 import moon from './../../assets/moon.svg'
-import twitterLight from './../../assets/twitter-light.svg'
-import twitterDark from './../../assets/twitter-dark.svg'
 import githubLight from './../../assets/github-light.svg'
 import githubDark from './../../assets/github-dark.svg'
 import LinkedinLight from './../../assets/linkedin-light.svg'
 import linkedinDark from './../../assets/linkedin-dark.svg'
-import curriculo from './../../assets/Currículo - Gisela.pdf'
+import curriculo from './../../assets/Currículo_Gisela.pdf'
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
   const { theme, toggleTheme } = useTheme()
 
   const themeIcon = theme === 'light' ? sun : moon
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark
   const githubIcon = theme === 'light' ? githubLight : githubDark
   const linkedinIcon = theme === 'light' ? LinkedinLight : linkedinDark
 
@@ -36,9 +33,6 @@ function Hero() {
                 Frontend Developer
             </h2>
             <span>
-                <a href='https://www.threads.net/@g154gisa' target='_blank'>
-                    <img src={twitterIcon} alt="Threads icon" />
-                </a>
                 <a href='https://github.com/giselaAS' target='_blank'>
                     <img src={githubIcon} alt="GitHub icon" />
                 </a>
